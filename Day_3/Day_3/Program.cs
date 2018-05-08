c92c686d6fa5ef749fe293fec3567a6fb1c07fe5
Agnese <fodza@inbox.lv>
2018-05-08 20:39:53 +0300

    2.commitments
    int result;
            result = skaitlis1 + skaitlis2;
                return result;

--- Day_3/Day_3/Program.cs
+++ Day_3/Day_3/Program.cs
@@ -13,25 +13,44 @@ namespace Day_3
             int mainigais = Funkcija2();
             Console.WriteLine(mainigais);
             Funkcija();
+            Funkcija3(5, 6);
+            int mainigais2 = Funkcija4(12, 14);
+            Console.WriteLine(mainigais2);
             Console.ReadLine();
         }
 
         static void Funkcija()
-        { 
-            int a ,b ,c;
-            a = 7;
+        {
+            int a, b, c;
+            a = 4;
             b = 8;
+
             c = a + b;
             Console.WriteLine(c);
-            
+
         }
         static int Funkcija2()
         {
             int a, b, c;
             a = 7;
             b = 8;
+
             c = a + b;
+
             return c;
+        }
+        static void Funkcija3(int skaitlis1, int skaitlis2)
+        {
+            int c = skaitlis1 + skaitlis2;
+            Console.WriteLine(c);
+        }
+        static int Funkcija4(int skaitlis1, int skaitlis2) //"int" raksta jo vajag atgriezt, var rakstit ari " double" - tad, cipars ar komatu
+        {
+        int result;
+        result = skaitlis1 + skaitlis2;
+            return result;
         }
     }
 }
+
+
